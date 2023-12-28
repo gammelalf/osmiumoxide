7a5275d783724949434cd6fa756b87c18663fe57
gammelalf <gammelalf@hopfen.space>
2023-12-28 15:05:37 +0100

    Extracted common parts from read, read_par

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,7 @@ use log::{debug, error, trace, warn};
 use rayon::prelude::*;
 use thiserror::Error;
 
-use crate::fileformat::{ParseError, ReadError};
+use crate::fileformat::{FileFormatReader, ParseError, RawBlock, ReadError};
 use crate::osmformat::{Block, DataBlock};
 
 pub mod fileformat;
@@ -29,53 +29,23 @@ pub mod proto {
 /// use [`fileformat::read_fileformat`] to iterate over the file's [`RawBlock`](fileformat::RawBlock)s
 /// and [`RawBlock::parse`] to decompress and parse them.
 pub fn read(path: impl AsRef<Path>) -> Result<impl Iterator<Item = DataBlock>, Error> {
-    let mut iter = fileformat::read_fileformat(File::open(path).map_err(Error::FileError)?);
-
-    let block = iter.next().ok_or(Error::MissingHeader)??;
-    let Block::Header(header) = block.parse()? else {
-        return Err(Error::MissingHeader);
-    };
-
-    trace!("File header: {header:#?}");
-    if let Some(feature) = header.unknown_required_features() {
-        return Err(Error::UnknownFeature(feature.to_string()));
-    }
-
-    Ok(iter.take_while(Result::is_ok).filter_map(|result| {
-        let raw = match result {
-            Ok(raw) => raw,
-            Err(err) => {
-                error!("Failed to read file");
-                debug!("Failed to read file: {err}");
-                return None;
-            }
-        };
-        let block = match raw.parse() {
-            Ok(block) => block,
-            Err(err) => {
-                error!("Failed to parse block");
-                debug!("Failed to parse block: {err}");
-                return None;
-            }
-        };
-        match block {
-            Block::Header(_) => {
-                warn!("Skipping header block");
-                None
-            }
-            Block::Data(block) => Some(block),
-            Block::Unknown(_) => {
-                warn!("Skipping unknown block");
-                None
-            }
-        }
-    }))
+    Ok(read_process_header(path.as_ref())?
+        .take_while(Result::is_ok)
+        .filter_map(read_process_block))
 }
 
 /// Read a `.osm.pbf` file and return an iterator over its blocks
 ///
 /// [`rayon`] version of [`read`]
 pub fn read_par(path: impl AsRef<Path>) -> Result<impl ParallelIterator<Item = DataBlock>, Error> {
+    Ok(read_process_header(path.as_ref())?
+        .par_bridge()
+        .take_any_while(Result::is_ok)
+        .filter_map(read_process_block))
+}
+
+/// Helper function used in `read...` to open the file and process its header
+fn read_process_header(path: &Path) -> Result<FileFormatReader<File>, Error> {
     let mut iter = fileformat::read_fileformat(File::open(path).map_err(Error::FileError)?);
 
     let block = iter.next().ok_or(Error::MissingHeader)??;
@@ -88,38 +58,38 @@ pub fn read_par(path: impl AsRef<Path>) -> Result<impl ParallelIterator<Item = D
         return Err(Error::UnknownFeature(feature.to_string()));
     }
 
-    Ok(iter
-        .par_bridge()
-        .take_any_while(Result::is_ok)
-        .filter_map(|result| {
-            let raw = match result {
-                Ok(raw) => raw,
-                Err(err) => {
-                    error!("Failed to read file");
-                    debug!("Failed to read file: {err}");
-                    return None;
-                }
-            };
-            let block = match raw.parse() {
-                Ok(block) => block,
-                Err(err) => {
-                    error!("Failed to parse block");
-                    debug!("Failed to parse block: {err}");
-                    return None;
-                }
-            };
-            match block {
-                Block::Header(_) => {
-                    warn!("Skipping header block");
-                    None
-                }
-                Block::Data(block) => Some(block),
-                Block::Unknown(_) => {
-                    warn!("Skipping unknown block");
-                    None
-                }
-            }
-        }))
+    return Ok(iter);
+}
+
+/// Helper function used in `read...` to process the stream of blocks
+fn read_process_block(result: Result<RawBlock, ReadError>) -> Option<DataBlock> {
+    let raw = match result {
+        Ok(raw) => raw,
+        Err(err) => {
+            error!("Failed to read file");
+            debug!("Failed to read file: {err}");
+            return None;
+        }
+    };
+    let block = match raw.parse() {
+        Ok(block) => block,
+        Err(err) => {
+            error!("Failed to parse block");
+            debug!("Failed to parse block: {err}");
+            return None;
+        }
+    };
+    match block {
+        Block::Header(_) => {
+            warn!("Skipping header block");
+            None
+        }
+        Block::Data(block) => Some(block),
+        Block::Unknown(_) => {
+            warn!("Skipping unknown block");
+            None
+        }
+    }
 }
 
 #[derive(Error, Debug)]
